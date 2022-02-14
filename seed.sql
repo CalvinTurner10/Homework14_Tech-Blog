ca1f7939fe1fc80f2809258d9bb9755c6da0a12f
Calvin Turner <turnerjrcalvin@gmail.com>
2022-02-14 01:14:27 -0500

    seed file

---
+++ seed.sql
@@ -0,0 +1,31 @@
+INSERT INTO employees_db.department
+
+VALUES
+    ("Sales")
+    ("Engineering")
+    ("Accounting")
+    ("Legal");
+
+INSERT INTO
+    employee_db.employee_role(title, salary, department_id)
+VALUES
+    ("Salesperson", 80000, 1),
+    ("Lead Engineer", 150000, 2),
+    ("Software Engineer", 120000, 2),
+    ("Account Manager," 160000, 3),
+    ("Accountant", 125000, 3),
+    ("Legal Team Lead", 250000, 4),
+    ("Lawyer"), 190000, 4);
+
+    INSERT INTO
+        employee_db.employee (first_name, last_name, role_id, manager_id)
+    VALUES
+        ("Mike", "Chan", 1, 3),
+        ("Ashley", "Rodriguez", 2, 2),
+        ("Kevin", "Tupik", 3, 2),
+        ("Kunal", "Singh", 4, 3),
+        ("Malia", "Brown", 5, 3),
+        ("Sarah", "Lourd", 6, 4),
+        ("Tom", "Allen", 7, 4),
+
+

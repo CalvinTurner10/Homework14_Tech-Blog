82283ddbcb2bdad6b47d7a70f005c3f24caff23b
Calvin Turner <turnerjrcalvin@gmail.com>
2022-04-14 14:02:52 -0400

    exports

--- models/blogs.js
+++ models/blogs.js
@@ -1,16 +1,44 @@
-const { Sequelize, Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/config');
 
-class Post extends Model {}
+class Blogs extends Model {}
 
-Post.init(
+Blogs.init(
   {
-    title: DataTypes.STRING,
-    body: DataTypes.STRING
+id :{
+  type: DataTypes.INTEGER,
+  allowNull: false,
+  primaryKey: true,
+  autoIncrement: true,
+},
+name: {
+  type: DataTypes.STRING,
+  allowNull: false,
+},
+description: {
+  type: DataTypes.STRING,
+},
+date_created:{
+  type: DataTypes.Date,
+  allowNull: false,
+  defaultValue:DataTypes.NOW,
+},
+user_id:{
+  type: DataTypes.INTEGER,
+  references:{
+    module: 'user',
+    key: 'id',
+  },
+},
   },
   {
-    sequelize
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    moduleName : 'blogs',
   }
 );
+ 
 
-module.exports = Post;
+module.exports = Blogs;

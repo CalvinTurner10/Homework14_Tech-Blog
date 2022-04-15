14e7debf98627713a2ff044cfb0217b9f6dbaf76
Calvin Turner <turnerjrcalvin@gmail.com>
2022-04-14 21:29:52 -0400

    deploy

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,23 +1,38 @@
+const { response } = require("express");
+
 const commentFormHandler = async function(event) {
   event.preventDefault();
 
-  const postId = document.querySelector('input[name="post-id"]').value;
-  const body = document.querySelector('textarea[name="comment-body"]').value;
+  const name = document.querySelector('input[name="post-id"]').value;
+  const description = document.querySelector('textarea[name="comment-body"]').value;
 
-  if (body) {
+  if (name && description) {
     await fetch('/api/comment', {
       method: 'POST',
-      body: JSON.stringify({ text, blogs_id
- }),
+      body: JSON.stringify({ name, description }),
       headers: {
         'Content-Type': 'application/json'
-      }
+      },
     });
+    if(response.ok){
+      document.location.replace('/newprofile');
+    } else{
+      alert('failed')
+    }
 
-    document.location.reload();
   }
 };
+ const deleteButtonHandler =async (event) =>{
+   if (event.target.hasAttribute('data-id')){
+     const id = event.target.getAttribute('data-id');
 
-document
-  .querySelector('#new-comment-form')
-  .addEventListener('submit', commentFormHandler);
+     const response =await fetch (`api/blog/${id}`,{
+       method:'Delete',
+     });
+     if(reponse.ok){
+       document.location.replace('/blog');
+     } else {
+       alert('failed')
+     }
+   }
+ };
